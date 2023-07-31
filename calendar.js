fff9d8fb5f6dda9d5a9552bb038b8f6c52e42f53
haruki-smz <141037242+haruki-smz@users.noreply.github.com>
2023-07-31 22:31:48 +0900

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,133 @@
+const week = ["日", "月", "火", "水", "木", "金", "土"];
+const today = new Date();
+// 月末だとずれる可能性があるため、1日固定で取得
+var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+
+// 初期表示
+window.onload = function () {
+    showProcess(today, calendar);
+};
+
+function prev() {
+  showDate.setMonth(showDate.getMonth() - 1);
+  showProcess(showDate);
+}
+
+function next() {
+  showDate.setMonth(showDate.getMonth() + 1);
+  showProcess(showDate);
+}
+
+function showProcess(date) {
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
+
+  var calendar = createProcess(year, month);
+  document.querySelector('#calendar').innerHTML = calendar;
+
+  
+
+  // 日付のセルにクリックイベントを追加する
+  var dayCells = document.querySelectorAll('.day');
+  dayCells.forEach(function(cell) {
+      cell.addEventListener('click', function() {
+          var clickedDate = parseInt(cell.textContent, 10);
+          showAlert(clickedDate);
+      });
+  });
+}
+
+function createProcess(year, month) {
+  var calendar = "<table><tr class='dayOfWeek'>";
+  for (var i = 0; i < week.length; i++) {
+      calendar += "<th>" + week[i] + "</th>";
+  }
+  calendar += "</tr>";
+
+  // 以下の部分は既存のコード（省略）...
+}
+
+// 以下の部分にアラートを表示するための関数を追加
+function showAlert(clickedDate) {
+  var diffDays = clickedDate - today.getDate();
+  if (diffDays === 0) {
+      alert("今日です！");
+  } else if (diffDays > 0) {
+      alert(diffDays + "日後です！");
+  } else {
+      alert(Math.abs(diffDays) + "日前です！");
+  }
+}
+
+// 前の月表示
+function prev(){
+    showDate.setMonth(showDate.getMonth() - 1);
+    showProcess(showDate);
+}
+
+// 次の月表示
+function next(){
+    showDate.setMonth(showDate.getMonth() + 1);
+    showProcess(showDate);
+}
+
+// カレンダー表示
+function showProcess(date) {
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
+
+    var calendar = createProcess(year, month);
+    document.querySelector('#calendar').innerHTML = calendar;
+}
+
+// カレンダー作成
+function createProcess(year, month) {
+    // 曜日
+    var calendar = "<table><tr class='dayOfWeek'>";
+    for (var i = 0; i < week.length; i++) {
+        calendar += "<th class='dayOfWeek'>" + week[i] + "</th>";
+    }
+    calendar += "</tr>";
+
+    var count = 0;
+    var startDayOfWeek = new Date(year, month, 1).getDay();
+    var endDate = new Date(year, month + 1, 0).getDate();
+    var lastMonthEndDate = new Date(year, month, 0).getDate();
+    var row = Math.ceil((startDayOfWeek + endDate) / week.length);
+
+    // 1行ずつ設定
+    for (var i = 0; i < row; i++) {
+        calendar += "<tr>";
+        // 1colum単位で設定
+        for (var j = 0; j < week.length; j++) {
+            if (i == 0 && j < startDayOfWeek) {
+                // 1行目で1日まで先月の日付を設定
+                calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
+            } else if (count >= endDate) {
+                // 最終行で最終日以降、翌月の日付を設定
+                count++;
+                calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
+            } else {
+                // 当月の日付を曜日に照らし合わせて設定
+                count++;
+                if(year == today.getFullYear()
+                  && month == (today.getMonth())
+                  && count == today.getDate()){
+                    calendar += "<td class='today'>" + count + "</td>";
+                } else {
+                    calendar += "<td>" + count + "</td>";
+                }
+            }
+        }
+        calendar += "</tr>";
+    }
+    return calendar;
+}
+
+// 今日に戻るボタンをクリックしたときの処理
+function goToToday() {
+    showDate = new Date(); // showDateを今日の日付に設定
+    showProcess(showDate); // カレンダーを今日に更新
+}
